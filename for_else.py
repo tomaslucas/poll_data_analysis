917d4e30f13b740c5722ad8b53c1414f39da5f7a
Tomás Lucas <tomaslucas@gmail.com>
2022-08-14 20:21:17 +0200

    Base

---
+++ for_else.py
@@ -0,0 +1,9 @@
+# https://book.pythontips.com/en/latest/for_-_else.html
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print( n, 'equals', x, '*', n/x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
